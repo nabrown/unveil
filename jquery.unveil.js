839f49044651e6363411c67c6006559a4e66dd70
Nora Brown <nabrown78@gmail.com>
2019-01-17 21:33:22 -0500

    frontmatter edits

--- jquery.unveil.js
+++ jquery.unveil.js
@@ -1,7 +1,6 @@
 /**
  * jQuery Unveil
  * A very lightweight jQuery plugin to lazy load images
- * http://luis-almeida.github.com/unveil
  *
  * Licensed under the MIT license.
  * Copyright 2013 Luís Almeida
